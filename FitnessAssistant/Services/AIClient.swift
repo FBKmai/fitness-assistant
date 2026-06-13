c8b208ab0dd35c356840c3cdd8d66e606547d2d0
FBK <756933290@qq.com>
2026-06-13 21:53:43 +0800

    Fix CI build service imports

--- FitnessAssistant/Services/AIClient.swift
+++ FitnessAssistant/Services/AIClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 enum AIClientError: LocalizedError {
     case missingAPIKey
--- FitnessAssistant/Services/NotificationScheduler.swift
+++ FitnessAssistant/Services/NotificationScheduler.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 import UserNotifications
 
 final class NotificationScheduler: ObservableObject {
